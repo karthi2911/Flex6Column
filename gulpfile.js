03e73a80c52b42e1254a4b42755088b3760aaa1e
sathyar27 <tosathyar@gmail.com>
2018-01-17 18:21:50 +0530

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+/* Task to compile less */
+gulp.task('compile-less', function() {  
+  gulp.src('./asset/less/style.less')
+    .pipe(sourcemaps.init())  
+    .pipe(less())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./asset/css/'));
+}); 
+/* Task to watch less changes */
+gulp.task('watch-less', function() {  
+  gulp.watch('./asset/less/**/*.less' , ['compile-less']);
+});
+ 
+gulp.task('serve', function () {
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }); 
+    gulp.watch("./asset/less/*.less").on("change", reload);
+    gulp.watch("./*.html").on("change", reload);
+});
+ 
+/* Task when running `gulp` from terminal */
+gulp.task('default', ['watch-less', 'serve', 'compile-less']);
